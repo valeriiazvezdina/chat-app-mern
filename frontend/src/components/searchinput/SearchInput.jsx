66b7e09bc0eddca37e106a88fc6e0e2f6a40c11d
Valeriia Zvezdina <valeriia.zvezdina@gmail.com>
2024-07-16 23:45:34 +0300

    feat: add search chat func

--- frontend/src/components/searchinput/SearchInput.jsx
+++ frontend/src/components/searchinput/SearchInput.jsx
@@ -1,9 +1,44 @@
+import { useState } from 'react';
+import useChat from '../../zustand/useChat';
+import useGetChats from '../../hooks/useGetChats';
+import toast from 'react-hot-toast';
+
 export default function SearchInput() {
+    const [search, setSearch] = useState('');
+    const { setSelectedChat } = useChat();
+    const { chats } = useGetChats();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        if (!search) return;
+
+        if (search.length < 3)
+            return toast.error('Search must be at least 3 characters');
+
+        const chat = chats.find((c) =>
+            c.fullName.toLowerCase().includes(search.toLowerCase())
+        );
+
+        if (chat) {
+            setSelectedChat(chat);
+            setSearch('');
+        } else {
+            toast.error('No such chat found');
+        }
+    };
+
     return (
         <>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label className="input border shadow-md rounded-full border-gray-400 flex items-center gap-2 m-3">
-                    <input type="text" className="grow" placeholder="Search" />
+                    <input
+                        type="text"
+                        className="grow"
+                        placeholder="Search"
+                        value={search}
+                        onChange={(e) => setSearch(e.target.value)}
+                    />
                     <button type="button">
                         <svg
                             xmlns="http://www.w3.org/2000/svg"
